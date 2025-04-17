47ac1b18f9cd0821dc6f50f34edf5e13b259b15a
DarKouto <danielcouto9@live.com>
2025-04-17 21:13:14 +0100

    count chars complete

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
+from stats import get_num_words, count_chars
+
 # GETS ALL THE TEXT FROM A FILE, AS LONG AS THE PATH IS SUPPLIED
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-# USES THE SPLIT METHOD ON THE STRING, WHICH SAVES EACH WORD AS AN ITEM ON A LIST
-# SO IF WE GET THE LENGHT OF THAT ARRAY, WE GET THE NUMBER OF WORDS   
-def get_num_words(text):
-    num_words_list = text.split()
-    num_words = len(num_words_list)
-    return num_words
 
 # SAVES THE TEXT OF THE FRANKENSTEIN BOOK ON THE FULL_TEXT VARIABLE
 # SAVES THE RESULT OF THE WORD COUNT OF FULL_TEXT VARIABLE ON THE TOTAL_WORDS VARIABLE
 def main():
     full_text = get_book_text("books/frankenstein.txt")
     total_words = get_num_words(full_text)
+    final = count_chars(full_text)
     print(f"{total_words} words found in the document")
+    print(final)
 
 main()
---
+++ stats.py
@@ -0,0 +1,20 @@
+# USES THE SPLIT METHOD ON THE STRING, WHICH SAVES EACH WORD AS AN ITEM ON A LIST
+# SO IF WE GET THE LENGHT OF THAT ARRAY, WE GET THE NUMBER OF WORDS   
+def get_num_words(text):
+    num_words_list = text.split()
+    num_words = len(num_words_list)
+    return num_words
+
+# CONVERTS ALL CHARS TO LOWER CASE
+# ADDS THEM TO THE DICT IF NOT FOUND WITH THE VALUE OF 1
+# INCREMENTS +1 IF THE KEY IS ALREADY THERE
+
+def count_chars(text):
+    text_lower = text.lower()
+    count_dict = {}
+    for char in text_lower:
+        if char not in count_dict:
+            count_dict[char] = 1
+        else:
+            count_dict[char] += 1
+    return count_dict
