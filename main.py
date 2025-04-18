4dd13ee60c779e6a20fad283163466fbf22ef40b
DarKouto <danielcouto9@live.com>
2025-04-18 15:27:29 +0100

    implemented sys.argv for all the book paths

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import sys # IMPORTS A SYSTEM MODULE, TO ACCESS FILE PATHS AND SUCH
 from stats import get_num_words, count_chars, sort_chars
+
+
 
 # GETS ALL THE TEXT FROM A FILE, AS LONG AS THE PATH IS SUPPLIED
 def get_book_text(path):
@@ -6,7 +9,16 @@ def get_book_text(path):
         return f.read()
 
 def main():
-    path_to_book = "books/frankenstein.txt"
+
+    # IF I TYPE PYTHON MAIN.PY, THEN SYS.ARGV = [PYTHON MAIN.PY]
+    # IF I TYPE PYTHON MAIN.PY BOOKS/FRANKENSTEIN.TXT, THEN SYS.ARGV = [PYTHON MAIN.PY, BOOKS/FRANKENSTEIN.TXT]
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    # THIS MEANS THAT THE INDEX 1 OF sys.argv
+    path_to_book = sys.argv[1]
+
 
     # CAPTURES THE RETURN OF THE get_book_text FUNCTION ON THE FULL_BOOK VARIABLE
     full_book = get_book_text(path_to_book)
