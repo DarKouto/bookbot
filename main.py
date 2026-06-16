e43a5b8f43b755a083d3ce25aa6dd65156d57f14
DarKouto <danielcouto9@live.com>
2026-06-16 19:06:23 +0100

    updated to comply with new boot.dev exercise

--- main.py
+++ main.py
@@ -1,49 +1,27 @@
-import sys # IMPORTS A SYSTEM MODULE, TO ACCESS FILE PATHS AND SUCH
-from stats import get_num_words, count_chars, sort_chars
+from stats import get_num_words, count_chars, chars_dict_to_sorted_list
 
-
-
-# GETS ALL THE TEXT FROM A FILE, AS LONG AS THE PATH IS SUPPLIED
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-
-    # IF I TYPE PYTHON MAIN.PY, THEN SYS.ARGV = [PYTHON MAIN.PY]
-    # IF I TYPE PYTHON MAIN.PY BOOKS/FRANKENSTEIN.TXT, THEN SYS.ARGV = [PYTHON MAIN.PY, BOOKS/FRANKENSTEIN.TXT]
-    if len(sys.argv) < 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
     
-    # THIS MEANS THAT THE INDEX 1 OF sys.argv
-    path_to_book = sys.argv[1]
-
-
-    # CAPTURES THE RETURN OF THE get_book_text FUNCTION ON THE FULL_BOOK VARIABLE
+    path_to_book = "books/frankenstein.txt"
     full_book = get_book_text(path_to_book)
-    
-    # CAPTURES THE RETURN OF THE get_num_words FUNCTION ON THE TOTAL_WORDS VARIABLE
     total_words = get_num_words(full_book)
-    
-    # CAPTURES THE RETURN of the count_chars FUNCTION ON THE CHAR_COUNT VARIABLE
     char_count = count_chars(full_book)
-
-    #CAPURES THE RETURN OF THE sort_chars FUNCTION ON THE SORTED_CHARS VARIABLE
-    sorted_chars = sort_chars(char_count)
+    sorted_list = chars_dict_to_sorted_list(char_count)
     
-    # PRINTS
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path_to_book}...")
     print("----------- Word Count ----------")
     print(f"Found {total_words} total words")
     print("--------- Character Count -------")
-    
-    for char_dict in sorted_chars:
-        if char_dict["char"].isalpha() == True:
-            character = char_dict["char"]  
-            count = char_dict["count"]
-            print(f"{character}: {count}")
+
+    for char, count in sorted_list:
+        if char.isalpha() == True:
+            new_tpl=(char,count)
+            print(new_tpl)
 
     print("============= END ===============")
 
--- stats.py
+++ stats.py
@@ -1,13 +1,8 @@
-# USES THE SPLIT METHOD ON THE STRING, WHICH SAVES EACH WORD AS AN ITEM ON A LIST
-# SO IF WE GET THE LENGHT OF THAT LIST, WE GET THE NUMBER OF WORDS   
 def get_num_words(text):
     num_words_list = text.split()
     num_words = len(num_words_list)
     return num_words
 
-# CONVERTS ALL CHARS TO LOWER CASE
-# ADDS THEM TO THE DICT IF NOT FOUND WITH THE VALUE OF 1
-# INCREMENTS +1 IF THE KEY IS ALREADY THERE
 def count_chars(text):
     text_lower = text.lower()
     count_dict = {}
@@ -18,17 +13,22 @@ def count_chars(text):
             count_dict[char] += 1
     return count_dict
 
-# TAKES THE CHAR:COUNT DICITIONARY AS AN ARGUMENT
-# CONVERTS IT TO A LIST, BECAUSE SORT() CAN ONLY BE USED ON LISTS
-# THE LIST CONTAINS SEVERAL DICTIONARIES, ONE FOR EACH CHARACTER WITH COUNTS
 def sort_chars(dict):
     sorted_list = []
-
     for char, count in dict.items():
         sorted_list.append({"char": char, "count": count})
     
     def sorting_key(dict2):
         return dict2["count"]
-
+    
     sorted_list.sort(reverse=True, key=sorting_key)
     return sorted_list
+
+def sort_on(tpl):
+    return (tpl[1])
+
+def chars_dict_to_sorted_list(count_dict):
+    new_list = []
+    for char, count in count_dict.items():
+        new_list.append((char,count))
+    return sorted(new_list, key=sort_on, reverse=True)
